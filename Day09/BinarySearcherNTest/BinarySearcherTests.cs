53a1eb7c7baf3387e4a004c68b3ff82068f1f2f5
AntonPavl <pavlenok.anton@gmail.com>
2016-07-17 17:05:26 +0300

    BinaryFinished

---
+++ Day09/BinarySearcherNTest/BinarySearcherTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+using Searches;
+using System.Collections;
+
+namespace BinarySearcherNTest
+{
+    [TestFixture]
+    public class BinarySearcherTests
+    {
+        [Test]
+        public void Search_intarray_value_2_result_2()
+        {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int pos = BinarySearcher.Search(array, 2);
+            Assert.AreEqual(pos, 2);
+        }
+
+        [Test]
+        public void Search_stringarray_value_2_result_2()
+        {
+            string[] sarray = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+            int pos = BinarySearcher.Search(sarray, "2");
+            Assert.AreEqual(pos, 2);
+        }
+
+
+        struct Temp : IComparable
+        {
+            public int ID;
+            public Temp(int id)
+            {
+                ID = id; 
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (!(obj is Temp)) throw new ArgumentException();
+                return ID.CompareTo(((Temp)obj).ID);
+            }
+
+            public override string ToString()
+            {
+                return ID.ToString();
+            }
+
+        }
+
+        public class Tempcomparer : IComparer
+        {
+            public int Compare(object a, object b)
+            {
+                if (ReferenceEquals(a, b)) return 0;
+                if (ReferenceEquals(a, null)) return -1;
+                if (ReferenceEquals(b, null)) return 1;
+                if (((Temp)a).ID > (int)b) return 1;
+                if (((Temp)a).ID < (int)b) return -1;
+                return 0;
+            }
+        }
+
+
+        [Test]
+        public void Search_structID_value_10_result_3()
+        {
+            Temp[] tarray = new Temp[4];
+            tarray[0] = new Temp(3);
+            tarray[1] = new Temp(6);
+            tarray[2] = new Temp(9);
+            tarray[3] = new Temp(10);
+            int pos = BinarySearcher.Search(tarray,10,new Tempcomparer());
+            Assert.AreEqual(pos, 3);
+        }
+
+        [Test]
+        public void Search_struct_struct_3()
+        {
+            Temp[] tarray = new Temp[4];
+            tarray[0] = new Temp(3);
+            tarray[1] = new Temp(6);
+            tarray[2] = new Temp(9);
+            tarray[3] = new Temp(10);
+            Temp search = new Temp(10);
+            int pos = BinarySearcher.Search(tarray, search);
+            Assert.AreEqual(pos, 3);
+        }
+    }
+}
---
+++ Day09/Searches/BinarySearcher.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searches
+{
+    public static class BinarySearcher
+    {
+        public sealed class Comparer : IComparer
+        {
+            public static readonly Comparer Default = new Comparer();
+            public Comparer()
+            {
+
+            }
+
+            public int Compare(Object a, Object b)
+            {
+                if (a == b) return 0;
+                if (a == null) return -1;
+                if (b == null) return 1;
+
+                IComparable ia = a as IComparable;
+                if (ia != null) return ia.CompareTo(b);
+
+                IComparable ib = b as IComparable;
+                if (ib != null) return -ib.CompareTo(a);
+                throw new ArgumentException("Argument_ImplementIComparable");
+            }
+
+        }
+
+        public static int Search(Array array,object obj) =>
+             BinarySearch(array, array.GetLowerBound(0), array.GetUpperBound(0)+1, obj,null);
+        public static int Search(Array array, object obj, IComparer comparer) =>
+            BinarySearch(array, array.GetLowerBound(0), array.GetUpperBound(0) + 1, obj, comparer);
+
+        private static int BinarySearch(Array array, int index, int length, Object value, IComparer comparer)
+        {
+            if (array == null) throw new ArgumentNullException("array");
+            int lb = array.GetLowerBound(0);
+            if (index < lb || length < 0) throw new ArgumentOutOfRangeException(index < lb ? "index" : "length");
+            if (array.Length - (index - lb) < length) throw new ArgumentException("Argument_InvalidOffLen");
+            if (array.Rank != 1) throw new RankException("Rank_MultiDimNotSupported");
+            if (comparer == null) comparer = Comparer.Default;
+
+            Object[] objArray = array as Object[];       
+            if (objArray != null)
+            {
+                return Body(index, length, (f) => objArray[f], value, comparer);
+            }
+            else
+            {
+                return Body(index, length, (f) => array.GetValue(f), value, comparer);
+            }
+        }
+        private static int Body(int index, int length, Func<int, object> @delegate, object value, IComparer comparer)
+        {
+            int lo = index;
+            int hi = index + length - 1;
+            while (lo <= hi)
+            {
+                int i = GetMedian(lo, hi);
+
+                int c;
+                try
+                {
+                    c = comparer.Compare(@delegate(i), value);
+                }
+                catch 
+                {
+                    throw new InvalidOperationException("InvalidOperation_IComparerFailed");
+                }
+                if (c == 0) return i;
+                if (c < 0)
+                {
+                    lo = i + 1;
+                }
+                else
+                {
+                    hi = i - 1;
+                }
+            }
+            return ~lo;
+        }
+
+        private static int GetMedian(int low, int hi)
+        {
+            if (low > hi) throw new ArgumentException();
+            if (hi - low < 0) throw new ArgumentException("Length overflow");
+            return low + ((hi - low) >> 1);
+        }
+    }
+}

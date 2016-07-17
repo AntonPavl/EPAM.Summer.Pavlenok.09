8d310f62193fa95532db7e4f5c97608a6d783593
AntonPavl <pavlenok.anton@gmail.com>
2016-07-17 15:08:47 +0300

    Timer Finished

---
+++ Day09/EventTimer/Timer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EventTimer
+{
+    public sealed class TimerEventData : EventArgs
+    {
+        private readonly string message;
+        private readonly string info;
+        public TimerEventData(string message, string info)
+        {
+            this.message = message;
+            this.info = info;
+        }
+
+        public string Message { get { return message; } }
+        public string Info { get { return info; } }
+    }
+
+    public class Timer
+    {
+        private System.Timers.Timer repeater;   
+        public event EventHandler<TimerEventData> Events = delegate { };
+        private TimerEventData eventData;
+        /// <summary>
+        /// Create Timer
+        /// </summary>
+        /// <param name="time">Time for a repeat</param>
+        /// <param name="data">Event Data</param>
+        /// <param name="isRepeat">Sets a value indicating event each time the interval elapses or only after the first time it elapses</param>
+        public Timer(int time = 1000,TimerEventData data = null,bool isRepeat = true)
+        {
+            repeater = new System.Timers.Timer(time);
+            repeater.AutoReset = isRepeat;
+            repeater.Elapsed += OnTimedEvent;
+            eventData = data;
+        }
+        /// <summary>
+        /// Start repeater
+        /// </summary>
+        public void StartTimer()
+        {
+            repeater.Enabled = true;
+        }
+        /// <summary>
+        /// Stop repeater
+        /// </summary>
+        public void StopTimer()
+        {
+            repeater.Enabled = false;
+        }
+
+        protected virtual void Sender(TimerEventData e)
+        {
+            EventHandler<TimerEventData> temp = Events;
+            if (temp != null)
+            {
+                temp(this, e);
+            }
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Sender(eventData);
+        }
+        /// <summary>
+        /// Set EventData 
+        /// </summary>
+        /// <param name="e">EventData that will be sent</param>
+        public void SetEventData(TimerEventData e)
+        {
+            eventData = e;
+        }
+    }
+}
---
+++ Day09/TimerTestConsole/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventTimer;
+namespace TimerTestConsole
+{
+    class Program
+    {
+        public class Person1
+        {
+            public Person1(Timer timer)
+            {
+                Register(timer);
+            }
+            public Person1()
+            {
+
+            }
+            private void Reaction(Object sender,TimerEventData ted)
+            {
+                Console.WriteLine($"I'm person 1");
+                Console.WriteLine($"Data message:{ted?.Message}");
+                Console.WriteLine($"Data extra info:{ted?.Info}");
+                Console.WriteLine($"--------------------------");
+            }
+            public void Unregister(Timer timer)
+            {
+                if (ReferenceEquals(timer, null)) throw new ArgumentNullException();
+                timer.Events -= Reaction;
+            }
+            public void Register(Timer timer)
+            {
+                if (ReferenceEquals(timer, null)) throw new ArgumentNullException();
+                timer.Events += Reaction;
+            }
+        }
+        public class Person2
+        {
+            public Person2(Timer timer)
+            {
+                Register(timer);
+            }
+            public Person2()
+            {
+
+            }
+            private void Reaction(Object sender, TimerEventData ted)
+            {
+                Console.WriteLine($"I'm person 2");
+                Console.WriteLine($"Data message:{ted?.Message}");
+                Console.WriteLine($"Data extra info:{ted?.Info}");
+                Console.WriteLine($"--------------------------");
+            }
+            public void Unregister(Timer timer)
+            {
+                timer.Events -= Reaction;
+            }
+            public void Register(Timer timer)
+            {
+                timer.Events += Reaction;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var t = new Timer(200);
+            var t2 = new Timer(1000); t2.SetEventData(new TimerEventData("RARELY","WoooW"));
+            var p1 = new Person1(t);
+            var p2 = new Person2(t);
+            p1.Register(t2);
+            p2.Register(t2);      
+            t.StartTimer();
+            t2.StartTimer();
+            Console.ReadKey();
+            t.SetEventData(new TimerEventData("New DDOS!!","info"));
+            p1.Unregister(t);
+            Console.ReadKey();
+            t.StopTimer();
+            Console.ReadKey();
+        }
+    }
+}
